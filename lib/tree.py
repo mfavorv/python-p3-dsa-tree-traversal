187f07a7fd718e66a589259ccc38e021f9f513e9
Favor Vincent <vfavorm77@gmail.com>
2024-06-10 08:12:35 +0300

    traversal lab complete

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        if not self.root:
+            return None
+        return self._get_element_by_id_depth_first(self.root, id)
+
+    def _get_element_by_id_depth_first(self, node, id):
+         if node['id'] == id:
+            return node
+         for child in node['children']:
+            result = self._get_element_by_id_depth_first(child, id)
+            if result:
+                return result  
+         return None
